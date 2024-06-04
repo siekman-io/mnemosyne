4f1448c4ba0ea1a0d8f9f13257355a2f63330ab7
Jouke Siekman <github@siekman.io>
2024-06-04 12:59:36 +0200

    quick push

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//Credentials
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "databasename";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
